96dbd6a51412404d49f7fd619a831e3b21b06215
Margaret <Queen@Big-Macs-MacBook-Pro-149.local>
2014-11-04 11:30:34 -0600

    Files for dear Diary project and jquery exercise.

---
+++ public/js/diary.js
@@ -0,0 +1,7 @@
+$('.bodySize').click( function() {
+    $(this).toggleClass('on');
+});
+
+var comment = "<?= $_POST['feelings'] ?>"
+var alias = "<?= $_POST['alias'] ?>"
+
---
+++ public/js/site.js
@@ -0,0 +1,3 @@
+$(function() {
+            $('.codeup').css('border-bottom', 'red 2px solid');
+        });
